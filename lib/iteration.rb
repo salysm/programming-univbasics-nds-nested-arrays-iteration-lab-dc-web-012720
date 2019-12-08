b48684c0de809cf3fde18648e6ee809abf16d533
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 10:03:31 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,7 +1,7 @@
 def join_ingredients(src)
   array_1 = [['pepperoni', 'sausage'], ['green olives', 'green peppers'], ['onions', 'pineapple']]
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  join_ingredients = ("I love" array_1[0][1]
+  join_ingredients = ("I love" array_1[0][0] "and " array_1[0][1] "on my pizza")
   # Build a new Array that contains strings where each pair of foods is
   # inserted into this template:
   #
